fcdb8fc3bc23487ab1e199034987c497d13ed4a0
Harry <3010876946@qq.com>
2026-08-28 13:48:34 +0800

    Remove temp script

--- fix_subs.js
+++
@@ -1,5 +0,0 @@
-test
-
-const old = "            const from = data.from ? '出自 ' + data.from : ''\\n            typedJSFn.processSubtitle(data.hitokoto, from ? [from] : [])"
-const file = G:/1233/themes/butterfly/layout/includes/third-party/subtitle.pug
-
