423656dc3a612811208e4890a425cf298cb11ced
Harry <3010876946@qq.com>
2026-08-28 13:46:50 +0800

    Remove from attribution and add auto-rotate on typewriter complete

---
+++ fix_subs.js
@@ -0,0 +1,5 @@
+test
+
+const old = "            const from = data.from ? '出自 ' + data.from : ''\\n            typedJSFn.processSubtitle(data.hitokoto, from ? [from] : [])"
+const file = G:/1233/themes/butterfly/layout/includes/third-party/subtitle.pug
+
